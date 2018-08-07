c51419b4fcc052c2d8507fc36a6433b1d529e421
Molosnic Dumitru <dimasalmaz@gmail.com>
2018-08-07 04:19:48 +0300

    Add Animal Data assets.

---
+++ Assets/Source/Data/AnimalData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Data
+{
+    [CreateAssetMenu(fileName = "AnimalSprites", menuName = "Configs/Animal Sprites")]
+    public class AnimalData : ScriptableObject
+    {
+        public Sprite Sprites;
+
+        public Sprite GetRandomSprite()
+        {
+            return Sprites;
+        }
+    }
+}
