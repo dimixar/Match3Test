6bc3b0a542b8bb85a5c4fd55d76afd0a43beb65a
Molosnic Dumitru <dimasalmaz@gmail.com>
2018-08-07 04:20:57 +0300

    Add position 2d component and transform sync system.

---
+++ Assets/Source/ECS/Components/Position2D.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Position2D : MonoBehaviour {
+	public float2 Value;
+}
---
+++ Assets/Source/ECS/Systems/TransformSyncSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class TransformSyncSystem : ComponentSystem
+{
+	public struct Data
+	{
+		[ReadOnly] public Position2D Position;
+		public Transform Output;
+	}
+	
+	protected override void OnUpdate()
+	{
+		foreach (var entity in GetEntities<Data>())
+		{
+			float2 pos = entity.Position.Value;
+			entity.Output.position = new float3(pos.x, pos.y, 0);
+		}
+	}
+}
