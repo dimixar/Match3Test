310477eab272a176f19464cf5390e209437fcbad
Molosnic Dumitru <dimasalmaz@gmail.com>
2018-08-07 04:21:08 +0300

    Add game settings.

---
+++ Assets/Source/ECS/Components/Settings.cs
@@ -0,0 +1,23 @@
+﻿using Data;
+using UnityEngine;
+
+namespace ECS.Components
+{
+    public class Settings : MonoBehaviour
+    {
+        public AnimalData[] AnimalDatas;
+        public Vector2 BoardSize;
+        public Vector2 ElementSize;
+
+        public Vector2 StartPoint;
+
+        public GameObject AnimalPrefab;
+        public float FallStartY;
+        public float MoveSpeed = 10f;
+
+        public AnimalData GetRandomAnimalData()
+        {
+            return AnimalDatas[Random.Range(0, AnimalDatas.Length)];
+        }
+    }
+}
