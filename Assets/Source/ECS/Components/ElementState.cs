1739cc1fe1830d0691a04bf52aac36735be616a3
Molosnic Dumitru <dimasalmaz@gmail.com>
2018-08-08 04:29:53 +0300

    Add additional state flags for element

--- Assets/Source/ECS/Components/ElementState.cs
+++ Assets/Source/ECS/Components/ElementState.cs
@@ -5,5 +5,9 @@ namespace ECS.Components
     public class ElementState : MonoBehaviour
     {
         public bool IsMoved;
+        public bool IsMarkedForRemoval;
+        public bool IsRemoved;
+        public bool IsReadyForReuse;
+        public bool IsMarkedForAppearance;
     }
 }
