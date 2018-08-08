30cdae3ba776983ddb4b4b0b6340b5d79041ceb0
Molosnic Dumitru <dimasalmaz@gmail.com>
2018-08-08 04:31:41 +0300

    Add Appear element system.

---
+++ Assets/Source/ECS/Systems/AppearElementSystem.cs
@@ -0,0 +1,40 @@
+﻿using ECS.Components;
+using Unity.Entities;
+using UnityEngine;
+
+namespace ECS.Systems
+{
+    [UpdateAfter(typeof(RespawnElementSystem))]
+    public class AppearElementSystem : ComponentSystem
+    {
+        public struct AnimalGroup
+        {
+            public Transform Transform;
+            public ElementState State;
+        }
+
+        public struct SettingsGroup
+        {
+            public Settings Settings;
+        }
+        
+        protected override void OnUpdate()
+        {
+            var settings = GetEntities<SettingsGroup>()[0].Settings;
+            foreach (var animal in GetEntities<AnimalGroup>())
+            {
+                if (animal.State.IsMarkedForAppearance == false)
+                    continue;
+
+                animal.Transform.localScale = Vector3.Lerp(animal.Transform.localScale, Vector3.one,
+                    Time.deltaTime * settings.AppearSpeed);
+                if (animal.Transform.localScale != Vector3.one)
+                    continue;
+
+                animal.State.IsMarkedForAppearance = false;
+                animal.State.IsRemoved = false;
+                animal.State.IsReadyForReuse = false;
+            }
+        }
+    }
+}
